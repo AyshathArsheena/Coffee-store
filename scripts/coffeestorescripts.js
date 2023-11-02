f894100c1d8ec4bd327c36f169177f3db2857e76
C K <ayshath-arsheena.c-k@capgemini.com>
2023-11-02 13:02:56 +0530

    coffee-store

---
+++ scripts/coffeestorescripts.js
@@ -0,0 +1,14 @@
+var a = getElementById('myaddtocart1');
+var b = getElementById('myaddtocart2');
+var c = getElementById('myaddtocart3');
+
+b.onclick = function () {
+    alert("Your coffee was added to cart");
+}
+
+a.onclick = function () {
+    alert("Your coffee was added to cart");
+}
+c.onclick = function () {
+    alert("Your coffee was added to cart");
+}
